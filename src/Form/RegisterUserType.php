39dccdfd216f5040df9783d532a2eacd60e27b19
chafroud sabri <chafroud.sabri@hotmail.fr>
2024-09-12 14:33:05 +0200

    constrainte sur le formulaire user

--- src/Form/RegisterUserType.php
+++ src/Form/RegisterUserType.php
@@ -5,12 +5,14 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 class RegisterUserType extends AbstractType
 {
@@ -19,21 +21,45 @@ class RegisterUserType extends AbstractType
         $builder
             ->add('firstname', TextType::class, [
                 'label' => "Votre prénom",
+                'constraints' => [
+                    new Length([
+                        'min' => 2,
+                        'max' => 30
+                    ])
+                ],
+
                 'attr' => ['placeholder' => 'Indiquez votre prénom']
             ])
 
+
             ->add('lastname', TextType::class, [
                 'label' => "Votre nom",
+                'constraints' => [
+                    new Length([
+                        'min' => 2,
+                        'max' => 30
+                    ])
+                ],
+
                 'attr' => ['placeholder' => 'Indiquez votre nom']
             ])
+
 
             ->add('email', EmailType::class, [
                 'label' => "Votre adresse email",
                 'attr' => ['placeholder' => 'Indiquez votre adresse email']
             ])
 
+
             ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
+                'constraints' => [
+                    new Length([
+                        'min' => 4,
+                        'max' => 30
+                    ])
+                ],
+
                 'first_options'  => [
                     'label' => 'Votre mot de passe',
                     'attr' => 
@@ -51,6 +77,7 @@ class RegisterUserType extends AbstractType
                 'mapped' => false,  // pour pas faire le lien entre password dans l'entity et le formulaire plainPassword
             ])
             
+
             ->add('submit', SubmitType::class, [
                 'label' => "S'inscrire",
                 'attr' => [
@@ -64,6 +91,13 @@ class RegisterUserType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
+            'constraints' => [   // contrainte entity unique unique le champ email ne peux etre utiliser deux fois
+                new UniqueEntity([
+                    'entityClass' => User::class,
+                    'fields' => 'email'
+                ])
+            ],
+
             'data_class' => User::class,
         ]);
     }
