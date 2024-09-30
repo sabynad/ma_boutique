ee3b923da2c0f5d67c3b2db38310a5747f9f8b60
chafroud sabri <chafroud.sabri@hotmail.fr>
2024-09-30 13:47:37 +0200

    validation panier

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Classe\Cart;
 use App\Form\OrderType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -21,11 +23,45 @@ class OrderController extends AbstractController
         }
 
         $form = $this->createForm(OrderType::class, null, [
-            'addresses' => $addresses
+            'addresses' => $addresses,
+            'action' => $this->generateUrl('app_order_summary')
         ]);
 
         return $this->render('order/index.html.twig', [
             'deliverForm' => $form->createView(),
         ]);
     }
+
+    // 2eme etape tunnel d'achat
+    //  recap de la commande utilisateur 
+    // Insertion en base de donnees
+    // preparation du paiment vers Stripe
+    #[Route('/commande/recapitulatif', name: 'app_order_summary')]
+    public function add(Request $request, Cart $cart): Response
+    {
+
+        if ($request->getMethod() != 'POST') {
+            return $this->redirectToRoute('app_cart');
+        }
+
+        $form = $this->createForm(OrderType::class, null, [
+            'addresses' => $this->getUser()->getAddresses(),
+        
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // dd($form->getData());
+            // stocker les informations en bdd
+        }
+
+        return $this->render('order/summary.html.twig', [
+            'choices' => $form->getData(),
+            'cart' => $cart->getCart(),
+            'totalWt' => $cart->getTotalWt()
+        ]);
+    }
+
+
 }
